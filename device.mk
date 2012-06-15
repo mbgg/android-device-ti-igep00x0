2e3bd1a191970702b295413337a6c2a195e22369
Matthias Brugger <mbrugger@iseebcn.com>
2012-06-15 16:51:22 +0200

    Add wifi support to device
    This update adds wifi support for libertas to the device specific
    configuration files.
    
    Signed-off-by: Matthias Brugger <mbrugger@iseebcn.com>

--- device.mk
+++ device.mk
@@ -17,7 +17,35 @@
 
 PRODUCT_COPY_FILES := \
 	device/ti/igep00x0/init.rc:root/init.rc \
-	device/ti/igep00x0/vold.fstab:system/etc/vold.fstab
+	device/ti/igep00x0/vold.fstab:system/etc/vold.fstab \
+	device/ti/igep00x0/boot.wav:system/media/audio/ui/boot.wav
+
+# Wifi
+PRODUCT_COPY_FILES += \
+	frameworks/base/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+	frameworks/base/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml
+
+
+PRODUCT_COPY_FILES += \
+	device/ti/igep00x0/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf \
+	device/ti/igep00x0/rc.wifi:system/bin/rc.wifi \
+	device/ti/igep00x0/dhcpcd.conf:system/etc/dhcpcd/dhcpcd.config \
+	device/ti/igep00x0/libertas/LICENCE.libertas:system/etc/firmware/LICENCE.libertas \
+	device/ti/igep00x0/libertas/sd8686.bin:system/etc/firmware/sd8686.bin \
+	device/ti/igep00x0/libertas/sd8686_helper.bin:system/etc/firmware/sd8686_helper.bin
+
+# Bluetooth
+#PRODUCT_COPY_FILES += \
+#	device/ti/igep00x0/rc.bt:system/bin/rc.bt \
+#	device/ti/igep00x0/bt.psr:system/etc/bt.psr
+
+#PRODUCT_COPY_FILES += \
+#	device/ti/flashboard/android.hardware.bluetooth.xml:system/etc/permissions/android.hardware.bluetooth.xml \
+#	system/bluetooth/data/main.nonsmartphone.conf:system/etc/bluetooth/main.conf
+
+
+PRODUCT_COPY_FILES += \
+	device/ti/igep00x0/asound.conf:system/etc/asound.conf
 
 PRODUCT_PROPERTY_OVERRIDES := \
 	wifi.interface=wlan0 \
@@ -37,6 +65,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
 	libaudioutils
 
+#TODO
 PRODUCT_PACKAGES += \
         audio.primary.beagleboard \
         tinycap \
@@ -47,9 +76,12 @@ PRODUCT_PACKAGES += \
 	dhcpcd.conf \
 	TQS_D_1.7.ini \
 	calibrator
+#	hostapd.conf \
 
 # Filesystem management tools
 PRODUCT_PACKAGES += \
 	make_ext4fs
 
 $(call inherit-product, frameworks/base/build/tablet-dalvik-heap.mk)
+$(call inherit-product-if-exists, hardware/ti/wlan/mac80211/firmware/wl12xx_wlan_fw_products.mk)
+#$(call inherit-product-if-exists, hardware/ti/wpan/wl12xx-bluetooth/wl12xx_bt_products.mk)
